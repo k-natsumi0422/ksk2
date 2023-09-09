106dcce3c6b955429b48b033c3baa5c0c0ff4d41
milk_ <milk_@LAPTOP-QOOAVH68>
2023-09-09 23:01:42 +0900

    新規作成

---
+++ ksk2/src/test/java/com/example/demo/Ksk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ksk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
