0c60ae831f08917729c15c7e79daac3c28421909
shota-shiba <syota.0802.2@gmail.com>
2023-10-06 22:52:33 +0900

    10/6

--- ksk2/build.gradle.kts
+++ ksk2/build.gradle.kts
@@ -1,34 +1,35 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.1.3"
-	id("io.spring.dependency-management") version "1.1.3"
+java
+id("org.springframework.boot") version "3.1.3"
+id("io.spring.dependency-management") version "1.1.3"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+compileOnly {
+extendsFrom(configurations.annotationProcessor.get())
+}
 }
 
 repositories {
-	mavenCentral()
+mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+implementation("org.springframework.boot:spring-boot-starter-web")
+compileOnly("org.projectlombok:lombok")
+developmentOnly("org.springframework.boot:spring-boot-devtools")
+annotationProcessor("org.projectlombok:lombok")
+testImplementation("org.springframework.boot:spring-boot-starter-test")
+runtimeOnly ("org.postgresql:postgresql")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+useJUnitPlatform()
 }
