3583040ea74ced1962fce3871d45d70ca630fd8b
Yukina-bit <140829460+Yukina-bit@users.noreply.github.com>
2023-10-09 00:55:09 +0900

    DB接続成功

---
+++ ksk2/src/main/java/object/Customer.java
@@ -0,0 +1,16 @@
+package object;
+
+public class Customer {
+	
+	private int user_id;
+	
+	public int user_id() {
+		return user_id;
+	}
+	public void user_id(int user_id) {
+		this.user_id = user_id;
+	}
+	
+
+
+}
